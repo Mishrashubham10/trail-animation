b2503772587f8deea731614c8a1180d16648e6c1
Mishrashubham10 <shubhammishra10101998@gmail.com>
2023-03-15 11:07:39 +0530

    trail-animation

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body");
+
+body.addEventListener("mousemove", (e) => {
+    const xPos = e.offsetX;
+    const yPos = e.offsetY;
+
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px";
+    body.appendChild(spanEl);
+
+    setTimeout(() => {
+        spanEl.remove();
+    }, 3000);
+});
